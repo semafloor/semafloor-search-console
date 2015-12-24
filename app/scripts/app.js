c4ea5d2590d2b62e78e7c29bdaeb2f4d39300663
motss <wes_ngrongsen@hotmail.com>
2015-12-24 14:41:45 +0800

    :bowtie: first commit

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,16 +2,18 @@
   'use strict';
 
   // 4. Conditionally load webcomponentsjs polyfill.
-  var webComponentsSupported = ('registerElement' in document && 'import' in document.createElement('link') && 'content' in document.createElement('template'));
+  var webComponentsSupported = ('registerElement' in document &&
+  'import' in document.createElement('link') &&
+  'content' in document.createElement('template'));
 
   function finishLazyLoading() {
     // HTML Import's link element.
     var link = document.querySelector('#bundle');
 
     // 6. Remove skeleton.
-     function onImportLoaded() {
-      var skeleton = document.getElementById('skeleton');
-      skeleton.remove();
+    function onImportLoaded() {
+      // var skeleton = document.getElementById('skeleton');
+      // skeleton.remove();
 
       console.log('Elements are upgraded!');
     }
@@ -40,10 +42,9 @@
   }
 
   // run this when polyfill is needed.
-  window.addEventListener('WebComponentsReady', function(){
+  window.addEventListener('WebComponentsReady', function() {
     console.log('web-components-ready');
   });
-
 
   // We use Page.js for routing. This is a Micro
   // client-side router inspired by the Express router
@@ -56,9 +57,10 @@
    * Utility function to listen to an event on a node once.
    */
   function once(node, event, fn, args) {
-    var self = this;
-    var listener = function () {
-      fn.apply(self, args);
+    /* jshint validthis: true */
+    var _self = this;
+    var listener = function() {
+      fn.apply(_self, args);
       node.removeEventListener(event, listener, false);
     };
     node.addEventListener(event, listener, false);
@@ -67,16 +69,12 @@
   /**
    * Routes
    */
-  page('/:category/list', function (ctx, next) {
+  // page('/:category/list', function (ctx, next) {
+  page('/:category/list', function(ctx) {
     console.log(ctx);
     var category = ctx.params.category;
 
     function setData() {
-      // if (err) {
-      //   blog.category = category;
-      //   blog.page = 'main';
-      //   return;
-      // }
 
       blog.category = category;
       blog.page = category;
@@ -89,37 +87,30 @@
     }else {
       setData();
     }
-
   });
 
-  page('*', function () {
-    console.log('Cant\'t find: ' + window.location.href + '. Redirected you to Home Page');
+  page('*', function() {
+    console.log('Cant\'t find: ' +
+    window.location.href +
+    '. Redirected you to Home Page');
     page.redirect('/profile/list');
   });
 
+  page({
+    hashbang: true
+  });
   // add #! before urls
-  try {
-    page({
-      hashbang: true
-    });
-  } catch (e) {
-    console.log(e);
-  }
+  // try {
+  // } catch (e) {
+  //   console.log(e);
+  // }
 
-  // Grab a reference to our auto-binding template
-  // and give it some initial binding values
-  // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
-  // var app = document.querySelector('#app');
-  //
   // app.displayInstalledToast = function() {
   //   // Check to make sure caching is actually enabled—it won't be in the dev environment.
   //   if (!document.querySelector('platinum-sw-cache').disabled) {
   //     document.querySelector('#caching-complete').show();
   //   }
   // };
-  //
-  // // Listen for template bound event to know when bindings
-  // // have resolved and content has been stamped to the page
   // app.addEventListener('dom-change', function() {
   //   console.log('Our app is ready to rock!');
   // });
@@ -128,43 +119,5 @@
   // window.addEventListener('WebComponentsReady', function() {
   //   // imports are loaded and elements have been registered
   // });
-  //
-  // // Main area's paper-scroll-header-panel custom condensing transformation of
-  // // the appName in the middle-container and the bottom title in the bottom-container.
-  // // The appName is moved to top and shrunk on condensing. The bottom sub title
-  // // is shrunk to nothing on condensing.
-  // addEventListener('paper-header-transform', function(e) {
-  //   var appName = document.querySelector('#mainToolbar .app-name');
-  //   var middleContainer = document.querySelector('#mainToolbar .middle-container');
-  //   var bottomContainer = document.querySelector('#mainToolbar .bottom-container');
-  //   var detail = e.detail;
-  //   var heightDiff = detail.height - detail.condensedHeight;
-  //   var yRatio = Math.min(1, detail.y / heightDiff);
-  //   var maxMiddleScale = 0.50;  // appName max size when condensed. The smaller the number the smaller the condensed size.
-  //   var scaleMiddle = Math.max(maxMiddleScale, (heightDiff - detail.y) / (heightDiff / (1-maxMiddleScale))  + maxMiddleScale);
-  //   var scaleBottom = 1 - yRatio;
-  //
-  //   // Move/translate middleContainer
-  //   Polymer.Base.transform('translate3d(0,' + yRatio * 100 + '%,0)', middleContainer);
-  //
-  //   // Scale bottomContainer and bottom sub title to nothing and back
-  //   Polymer.Base.transform('scale(' + scaleBottom + ') translateZ(0)', bottomContainer);
-  //
-  //   // Scale middleContainer appName
-  //   Polymer.Base.transform('scale(' + scaleMiddle + ') translateZ(0)', appName);
-  // });
-  //
-  // // Close drawer after menu item is selected if drawerPanel is narrow
-  // app.onDataRouteClick = function() {
-  //   var drawerPanel = document.querySelector('#paperDrawerPanel');
-  //   if (drawerPanel.narrow) {
-  //     drawerPanel.closeDrawer();
-  //   }
-  // };
-  //
-  // // Scroll page to top and expand header
-  // app.scrollPageToTop = function() {
-  //   document.getElementById('mainContainer').scrollTop = 0;
-  // };
 
 })(document);
